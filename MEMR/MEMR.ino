400758f53a8e837e6dfbca85c18b8f46f5a6db27
Hallowjack <magdadomonkos@gmail.com>
2024-01-17 18:10:45 +0100

    Update MEMR.ino
    Test version, not fit for deployment as no validation has been done

--- MEMR/MEMR.ino
+++ MEMR/MEMR.ino
@@ -1,9 +1,53 @@
-void setup() {
-  // put your setup code here, to run once:
+/*
 
+*/
+
+#include <SoftwareSerial.h>
+
+string knownNumber = "+36302125195";
+SoftwareSerial sshield(7, 8);
+
+void setup() {
+    Serial.begin(9600);
+    sshield.begin(9600);
+    
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+    string buffer, number;
+    if(buffer = getSerialString() != "")
+    {
+      if(number = extractPhoneNumber(buffer) != "")
+      {
+        if(strcmp(number, knownNumber))
+        {
+          digitalWrite(4, HIGH);
+          delay(300000);
+          digitalWrite(4, LOW);
+        }
+      }
+    }
+}
 
+string getSerialString()
+{
+  delay(500);
+  
+  while(sshield.available()) 
+  {
+    return(sshield.readString());//A szoftver soros portrol olvasott adat visszaadasa
+  }
+  return "";
+}
+
+String extractPhoneNumber(const String &iString)
+{
+    int sPos = inputString.indexOf("+CLIP: \"+") + 9; // A telefonszam kezdete
+    
+    int ePos = inputString.indexOf("\",", startPos); // A telefonszam vege
+    
+    if (startPos >= 0 && endPos >= 0) {
+        return iString.substring(sPos, ePos); // A konkret szam visszaadasa
+    }
+    return ""; // Ures string vissza, ha nincs szam
 }
